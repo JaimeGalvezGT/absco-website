20d20b8f16d996069b932ba5f43d66905f0bad21
JaimeGalvezGT <50521876+JaimeGalvezGT@users.noreply.github.com>
2026-03-13 11:36:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 AOS.init({
-
 duration:1000
+})
 
+const menu=document.getElementById("mobile-menu")
+const nav=document.querySelector(".nav-list")
+
+menu.addEventListener("click",()=>{
+nav.classList.toggle("active")
 })
