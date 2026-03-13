d4ef61063aebdee812b5df2e856085027d3949d3
JaimeGalvezGT <50521876+JaimeGalvezGT@users.noreply.github.com>
2026-03-13 11:22:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-window.addEventListener("scroll", function(){
+AOS.init({
 
-const navbar = document.querySelector(".navbar")
-
-if(window.scrollY > 50){
-navbar.style.background = "#000"
-}
+duration:1000
 
 })
